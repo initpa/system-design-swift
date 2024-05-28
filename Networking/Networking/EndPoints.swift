325f8f5212628b357855a46a9fdccfaf827282fc
initpa <nitpa.bh@gmail.com>
2024-05-28 17:38:24 +0530

    endpoint protocol

--- Networking/Networking/EndPoints.swift
+++ Networking/Networking/EndPoints.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol EndPoint {
+    /*
+     Example
+     1. HTTPS or HTTP
+     2. baseURL: "api.flicker.com"
+     3. path: /services/rest
+     4. parameters: [URLQueryItem(name: "api_key", value: API_KEY)]
+     5. memthod: "GET"
+     */
+    var scheme: String { get }
+
+    var baseURL: String { get }
+    
+    var path: String { get }
+    
+    var parameters: [URLQueryItem] { get }
+    
+    var memthod: String { get }
+}
