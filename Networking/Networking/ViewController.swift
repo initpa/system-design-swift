18f190f4f887270ac9dcf459371a506dd3ae966a
initpa <nitpa.bh@gmail.com>
2024-05-29 13:34:37 +0530

    basic network layer with api

--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -12,6 +12,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let getRequest = APIRequest(method: .get, path: "users")
+
+        APIClient().perform(getRequest) { (result) in
+            switch result {
+            case .success(let response):
+                if let response = try? response.decode(to: [User].self) {
+                    let users = response.body
+                    for user in users {
+                        print("Name: \(user.name), Email: \(user.email), City: \(user.address.city)")
+                    }
+                }
+                else {
+                    
+                    print("Failed to decode response")
+                }
+            case .failure:
+                print("Error perform network request")
+            }
+        }
     }
 
 
